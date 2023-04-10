32f844a50452a701dd160a4d0d23bfb41956d174
zyf <291721108@qq.com>
2023-04-10 17:55:16 +0800

    完善验证码状态

--- app/Models/UserSend.php
+++ app/Models/UserSend.php
@@ -9,6 +9,11 @@ class UserSend extends Model
 
     protected $table = 'gh_user_send';
 
+    /**
+     * type
+     */
+    const  USER_SEND_ONE = 1;  // 登录
+    const  USER_SEND_TWO = 2;  // 密码修改
 
     /**
      * 格式化时间
