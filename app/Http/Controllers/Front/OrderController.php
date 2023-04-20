3c1c3b7a9dc0bed735250fed346e6acafd48092b
zyf <291721108@qq.com>
2023-04-20 15:15:19 +0800

    订单修改

--- app/Http/Controllers/Front/OrderController.php
+++ app/Http/Controllers/Front/OrderController.php
@@ -20,11 +20,13 @@ class OrderController extends BaseController
      *
      * @param institution_id 必选 int 机构id
      * @param institution_type 必选 int 房间类型id
-     * @param start_date 必选 string 看房日期
-     * @param end_date 必选 string 联系人
+     * @param start_date 必选 string 开始时间
+     * @param end_date 必选 string 结束时间
      * @param order_phone 必选 int 联系方式
      * @param payment_method 必选 int 支付方式
-     * @param remark 必选 string 备注
+     * @param order_remark 非必选 string 备注
+     * @param contacts 必选 string 联系人
+     * @param contacts_card 必选 string 联系人身份证
      *
      * @return {"meta":{"status":200,"msg":"成功"},"data":[]}
      *
@@ -40,10 +42,12 @@ class OrderController extends BaseController
         $this->validate($request, [
             'institution_id'        => 'required|numeric',
             'institution_type'      => 'required|numeric',
-            'start_date'            => 'required|numeric',
-            'end_date'              => 'required|numeric',
+            'start_date'            => 'required',
+            'end_date'              => 'required',
             'order_phone'           => 'required|numeric',
             'payment_method'        => 'required|numeric',
+            'contacts'              => 'required',
+            'contacts_card'         => 'required',
         ]);
 
         $data = OrderService::placeAnOrder($request);
