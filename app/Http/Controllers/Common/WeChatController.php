905d04c207788f8d96a1819ffc6b085d86d10067
zyf <291721108@qq.com>
2023-04-24 08:49:57 +0800

    微信授权配置19

--- app/Http/Controllers/Common/WeChatController.php
+++ app/Http/Controllers/Common/WeChatController.php
@@ -9,27 +9,49 @@ use Overtrue\LaravelWeChat\Facade as EasyWeChat;
 class WeChatController extends BaseController
 {
 
+    /**
+     * @catalog API/微信
+     * @title 微信授权
+     * @description 微信授权
+     * @method post
+     * @url 47.92.82.25/api/auth
+     *
+     * @return {"code":200,"msg":"成功","data":[]}
+     *
+     * @return_param code int 状态吗(200:请求成功,404:请求失败)
+     * @return_param msg string 返回信息
+     * @return_param data array 返回数据
+     *
+     * @remark
+     * @number 6
+     */
     public function auth(Request $request)
     {
 
         $config = config('wechat.official_account.default');
         $app = Factory::officialAccount($config);
         $response = $app->oauth->scopes(array(env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES')))->redirect(env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK'));
-        $data = curlGet($response);
-        $data = json_decode($data);
-dd($data);
-        return $data;
+
+//        return redirect($response, 302, ['Cache-Control' => 'no-cache']);
+        return $response;
+
     }
 
     public function callback(Request $request)
     {
-        $code = $request->input('code');
+
+
+
         $config = config('wechat.official_account.default');
         $app = Factory::officialAccount($config);
 
         // 获取 OAuth 授权结果用户信息
         $oauth = $app->oauth;
-        $user = $oauth->userFromToken($code)->toArray();
+        // 获取 OAuth 授权结果用户信息
+        $code = "微信回调URL携带的 code";
+        $user = $oauth->userFromCode();
+
+        $_SESSION['wechat_user'] = $user->toArray();
 
         dd($user);
         // 这里可以将用户信息存入数据库或者做其他操作
@@ -37,5 +59,4 @@ dd($data);
 
         return '授权成功';
     }
-
 }
