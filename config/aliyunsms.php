5b2141fbb51ec473c01e749cd712a9e69e0bc208
zyf <291721108@qq.com>
2023-05-10 10:20:13 +0800

    新增订房申请

--- config/aliyunsms.php
+++ config/aliyunsms.php
@@ -5,6 +5,3 @@ return [
     'access_secret' => env('ALIYUN_SMS_AS', null),
     'sign_name'     => env('ALIYUN_SMS_SIGN_NAME', null)
 ];
-
-//LTAI5t5rmQ17MaK5H6MTJnPt
-//ngXahrhVSI4QiCyynQwd099iuwWkSp
