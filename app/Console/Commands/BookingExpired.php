84b2eea347bc2e1d2730b8683ff5ecb70d4ce5d5
zyf <291721108@qq.com>
2023-05-11 11:49:08 +0800

    预约超时状态更改

--- app/Console/Commands/BookingExpired.php
+++ app/Console/Commands/BookingExpired.php
@@ -2,8 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Booking;
-use App\Models\BookingRoom;
+use App\Models\Order;
 use Illuminate\Console\Command;
 
 class BookingExpired extends Command
@@ -11,44 +10,26 @@ class BookingExpired extends Command
     protected $signature = 'bookingExpired:ok';
 
     /**
-     * 修改项目状态
+     * 预约订单超时状态修改
      * @return void
      */
     public function handle()
     {
-        // 预约状态修改
-        $booking = Booking::where('orderState', '=', Booking::BOOKING_SYS_TYPE_ONE)->select()->get();
-
         // 判断预约超期状态
-        $bookingArr = [];
-        foreach ($booking as $v) {
+        $orderBooking = Order::where('status',Order::ORDER_SYS_TYPE_FOUR)->select()->get();
 
-            if (time() > strtotime(date('Y-m-d H:i:s', $v->arrireDate + 24 * 3600)) && $v->orderState != Booking::BOOKING_SYS_TYPE_TWO) {
-                $bookingArr[] = $v->id;
+        $orderBookingArr = [];
+
+        foreach ($orderBooking as $v) {
+            // 无押金预约
+            if (time() > strtotime(date('Y-m-d H:i:s', $v->visitDate + 24 * 3600))  || time() > strtotime(date('Y-m-d H:i:s', $v->end_date) && $v->status == Order::ORDER_SYS_TYPE_FOUR)) {
+                $orderBookingArr[] = $v->id;
             }
         }
 
-        if (!empty($bookingArr)) {
-            Booking::whereIn('id', $bookingArr)->update([
-                'orderState' => Booking::BOOKING_SYS_TYPE_ZERO
-            ]);
-        }
-
-        // 订房状态修改
-        $bookingRoom = BookingRoom::where('status', '=', BookingRoom::ROOM_SYS_TYPE_ONE)->select()->get();
-
-        // 判断预约超期状态
-        $roomArr = [];
-        foreach ($bookingRoom as $v) {
-
-            if ($v -> startDate > strtotime(date('Y-m-d H:i:s',$v -> startDate+24*3600)) && $v->status != BookingRoom::ROOM_SYS_TYPE_TWO) {
-                $roomArr[] = $v->id;
-            }
-        }
-
-        if (!empty($roomArr)) {
-            BookingRoom::whereIn('id', $roomArr)->update([
-                'status' => BookingRoom::ROOM_SYS_TYPE_ZERO
+        if (!empty($orderBookingArr)) {
+            Order::whereIn('id', $orderBookingArr)->update([
+                'status' => Order::ORDER_SYS_TYPE_ZERO
             ]);
         }
 
