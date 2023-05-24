c4323baff3fc2aa3e5cdb7eec918447f32c547a8
zyf <291721108@qq.com>
2023-05-24 17:37:07 +0800

    商家端订单详情

--- app/Console/Commands/OrderComSta.php
+++ app/Console/Commands/OrderComSta.php
@@ -18,7 +18,7 @@ class OrderComSta extends Command
     {
 
         // 预约状态修改
-        $order = Order::select()->get();
+        $order = Order::where('status',Order::ORDER_SYS_TYPE_FOUR)->select()->get();
         $orderArr = [];
 
         foreach ($order as $v) {
