2ee2b5d3876aa72a23ff8cdf16aef6f961e3a3ce
zyf <291721108@qq.com>
2023-06-12 10:10:28 +0800

    支付宝沙箱跳转

--- app/Http/Controllers/Common/LicenseController.php
+++ app/Http/Controllers/Common/LicenseController.php
@@ -7,6 +7,7 @@ use Alipay\EasySDK\Kernel\Config;
 use Alipay\EasySDK\Kernel\Factory;
 use Alipay\EasySDK\Kernel\Util\ResponseChecker;
 use Illuminate\Http\Request;
+use PHPUnit\Runner\Exception;
 use Yansongda\Pay\Pay;
 
 class LicenseController extends BaseController
@@ -63,29 +64,30 @@ class LicenseController extends BaseController
         }
     }
 
-
-    protected function getOptions()
+    public function getOptions()
     {
         $options = new Config();
         $options->protocol = 'https';
-        $options->gatewayHost = 'https://openapi.alipay.com/gateway.do';
+        $options->gatewayHost = 'openapi.alipaydev.com/gateway.do';
         $options->signType = 'RSA2';
 
-        $options->appId = config("alipay.app_id");
-
+        $options->appId = '9021000122682218';
         // 为避免私钥随源码泄露，推荐从文件中读取私钥字符串而不是写入源码中
-        $options->merchantPrivateKey = config("alipay.private_key");
+        $options->merchantPrivateKey = 'MIIEowIBAAKCAQEAiniGWPj5XIvQouMYUpyDR6P80gNHk6Trq1i+ZAGa87v0wj1q/rPIi6hrc5seUrNhnE5oMA/klT+9TCtPWXCEZAL6SfbuY41xykmI80pwv4i7PWmwEsEKRYOjX+Z4j6X0uYIPTIa+jTTYbdWs27DMXFPdNQvP+pOIrOirVGXLPrugjA4nd7NaEH1acE1poK7EQaQ7GKDFv2u8Po0e43Woq4dzOG2LU+xEFSO6ScBC1cDsKled+t+k1CtTk8yomGhD1umQuGjgAkGmp8OwslBOmfVctL2pnnUd5Bcf0rnsn/hH9sDd/UyMgoZscoqoZndwDHjrj1WUDupB/7g53p0g7QIDAQABAoIBAE5RStNJxmgEoDVwslIPOeUsKBN0TWiBb9XS9KRFkClo1k+CQ2DZuITc9iFFy8nEsWGhqyX75zJPAbbyDAgvLoIOeReadUyTNJfQLYhFQy3hnN1oSHDjA/c7NA1KokfE+nxtxk9nKqFdEUhWAVWkUoGp4URecPxts3Dwi+7JQEIzMdmFm0PmE/iPHcxv4iyjADeewR1NCvh9lS1FCMwiF0OG5esE15ZsBlcV99CO6Lx250puIu3/VEnmIWirNjZdRYTOc8+VllnvIPBLo/bLi2cRN6XYwNttNa2x/vOKWWEEPWNBxDtay+z9OgCCk20GtLKhFRBIBVkV0ogCXvm7vAECgYEAwNyNjwueye3jf7zJbXGnpQt4CayKNIMUDaB81+lDNjtRJIcDuxwY7rRMwtKDCtCCbbsIVzc4nchI6Ryfiz8+0GRdxAAAi8B7F/13iOLdru8aiMTJ2pXZHMoL+/c0Ja3zJovTiwCOM7seplXZFLyfQcs+Exhv1rpm9jPEsX/j9wECgYEAt82QEvrarmiRutoY0++6tdteCBR+N4ZObgeZWS51am+6QhwwKU1T7rwCVvF8C1/2W6wqgRL91Q5lPe8y0DFZmnDFdsbVEGsmC9WI1yIu3Q+p1fQbdeJo2QgIqcFdrBHsY5PjTnZ2Qzt+hEn0TNO+qA+WjRCLNn7ZFxaOF2aude0CgYBCX+WMNIiaoHeqb7O3KeVzhOX0FmCeP/p30iMFP+90y6dadekPzVS7WmwMpNyarTYmQ7dUJNokW1jUeZhjGQoqOFCY8xM9BaqXkBFCmCmJWhr8tRVvWCEXqmXDesmEqkBDpN3SOge2wBCFWIhUfbIlt6gOdFuRQCfNVfW8zPINAQKBgQCM5sjLxAJgMUmGNCtUTTKUttYe25bmec8mCi1EcJkSPxRKGdHR17XADeC9ReIR1j7Fh/YNfMGZ0q9KC1SoxSwreMfnhgVP0NQZvlhok4vZa2iG29sxQ5+cwMvjVpi4kATnUTevrqB6aeFTCF5/htNhgpGnEkemPxes212SEuUrAQKBgAH80grAbSkO6HfSpqJMhc8MJ54dOttXGzTQ+/XqFRlCgP0e3MWI9QO4BiglZra1HmLC8pW3Gt9FmatVmkoFz8rESWnP9jFRkRzCIq8wvEL+TLMzn2uvwXfgVgLi0g5kN4JFEsZt/ugG4Ldcl5QKE30m/z5lQFR9D6dbbQhY0jiy';
 
-//        $options->alipayCertPath = '<-- 请填写您的支付宝公钥证书文件路径，例如：/foo/alipayCertPublicKey_RSA2.crt -->';
-//        $options->alipayRootCertPath = '<-- 请填写您的支付宝根证书文件路径，例如：/foo/alipayRootCert.crt" -->';
-//        $options->merchantCertPath = '<-- 请填写您的应用公钥证书文件路径，例如：/foo/appCertPublicKey_2019051064521003.crt -->';
+        $publicPath = base_path('public/');
+        $options->alipayCertPath        = $publicPath.'certificate/alipayPublicCert.crt';
+        $options->alipayRootCertPath    = $publicPath.'certificate/alipayRootCert.crt';
+        $options->merchantCertPath      = $publicPath.'certificate/appPublicCert.crt';
 
         //注：如果采用非证书模式，则无需赋值上面的三个证书路径，改为赋值如下的支付宝公钥字符串即可
-         $options->alipayPublicKey = config("alipay.public_key");
-
+        // $options->alipayPublicKey = '<-- 请填写您的支付宝公钥，例如：MIIBIjANBg... -->';
         //可设置异步通知接收服务地址（可选）
-//        $options->notifyUrl = "https://openapi-sandbox.dl.alipaydev.com/gateway.do";
+        //如果需要使用文件上传接口，请不要设置该参数
+//        $options->notifyUrl = "<-- 请填写您的支付类接口异步通知接收服务地址，例如：https://www.test.com/callback -->";
 
+        //可设置AES密钥，调用AES加解密相关接口时需要（可选）
+//        $options->encryptKey = "<-- 请填写您的AES密钥，例如：aa4BtZ4tspm2wnXLb1ThQA== -->";
         return $options;
     }
 
@@ -93,33 +95,60 @@ class LicenseController extends BaseController
     public function create(Request $request)
     {
         // 创建新的订单，省略具体实现
-
-        // 推送通知到商家手机上
-
-        Factory::setOptions($this->getOptions());
+        $factory = Factory::setOptions(self::getOptions());
 
         try {
             //2. 发起API调用（以支付能力下的统一收单交易创建接口为例）
-            $result = Factory::payment()->common()->create("iPhone6 16G", "20200326235526001", "88.88", "2088002656718920");
+//            $result = Factory::payment()->App()->pay("iPhone6 16G", "2020******5526001", "88.88");
+            $payUrl = $factory::payment()->app()->pay("iPhone6 16G", "2020******5526001", "88.88");
             $responseChecker = new ResponseChecker();
+
             //3. 处理响应或异常
-            if ($responseChecker->success($result)) {
-                echo "调用成功". PHP_EOL;
+            if ($responseChecker->success($payUrl)) {
+                $data = urldecode($payUrl->body);
+                $parameters = [];
+                parse_str($data, $parameters);
+                // 构建支付请求URL
+                $url = 'alipay://platformapi/startapp?appId=' . $parameters['app_id'] . '&timestamp=' . urlencode($parameters['timestamp']) . '&bizContent=' . urlencode($parameters['biz_content']) . '&sign=' . urlencode($parameters['sign']) . '&signType=' . $parameters['sign_type'] . '&appCertSn=' . $parameters['app_cert_sn'] . '&alipayRootCertSn=' . $parameters['alipay_root_cert_sn'];
+                // 发送重定向响应
+                header('Location: ' . $url);
+//                echo "调用成功". PHP_EOL;
             } else {
-                echo "调用失败，原因：". $result->msg."，".$result->subMsg.PHP_EOL;
+                echo "调用失败，原因：". $payUrl->msg."，".$payUrl->subMsg.PHP_EOL;
             }
+die();
+            $payParams = $result->body; // 假设 $result 是支付宝支付接口的响应
+
+            // 处理支付结果
+            if (!empty($payParams)) {
+                // 解析响应内容
+                parse_str($payParams, $parsedResponse);
+
+                // 提取参数值
+                $appId = $parsedResponse['app_id'];
+                $timestamp = $parsedResponse['timestamp'];
+                $signType = $parsedResponse['sign_type'];
+                $bizContent = $parsedResponse['biz_content'];
+                $sign = $parsedResponse['sign'];
+
+                $payUrl = Factory::payment()->common()->create($appId, $timestamp, $signType, $bizContent, $sign);
+                // 返回支付链接给移动应用
+                echo $payUrl;
+            } else {
+                // 支付请求失败，处理错误信息
+                $errorMessage = $response->msg ?? 'Unknown error';
+
+                // 返回错误信息给移动应用
+                echo $errorMessage;
+            }
+
         } catch (Exception $e) {
             echo "调用失败，". $e->getMessage(). PHP_EOL;;
         }
 
 
-
-
-
-
-
-
         die();
+        // 推送通知到商家手机上
         $order = [
             'out_trade_no' => 'order_no', // 自定义的订单号
             'total_amount' => '0.01', // 支付金额
@@ -143,6 +172,7 @@ class LicenseController extends BaseController
         $publicKey = openssl_get_publickey($formattedPublicKey);
         $errorMsg = openssl_error_string();
 
+dd($errorMsg);
         if ($publicKey === false) {
             // 公钥加载失败
             return 'error';
